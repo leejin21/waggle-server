5a7bf37e59389d05d235cd0ed08c50fdbe59d3d6
jin lee <leejin21c@gmail.com>
2020-11-14 00:13:59 +0900

    uesr auth: login and register

--- src/api/user.router.js
+++ src/api/user.router.js
@@ -10,22 +10,76 @@
 var express = require('express');
 var router = express.Router();
 
+var jwt = require('jsonwebtoken');
+
+//////////////////////////////////////////////////
+//* USERS DATA: TEMPORARY CODE
+const users = [
+    {
+        email: 'john',
+        pw: '1234',
+        phone: '010-1111-1111',
+        sex: 'F',
+        birth: new Date(1991,0,1),
+        name: '이존'
+    }, {
+        email: 'anna',
+        pw: '1234',
+        phone: '010-2222-2222',
+        sex: 'F',
+        birth: new Date(1992,1,2),
+        name: '이애나'
+    }
+];
+
+const accesstokensecret = "thisisaccesstokensecret"
+
 //////////////////////////////////////////////////
 //* USER ROUTER
+// TODO services로 세부 로직들 옮기기
 
 // user/register
 router.post('/register', function(req, res){
-    console.log(req.body)
-    res.sendStatus(201);
+
+    const {email, pw, phone, sex, birth, name} = req.body;
+    
+    // email 중복 확인(실존 이메일인 지는 문의사항 관련해서 alert 주기로 하기)
+    // 나머지 데이터 validate 확인하기
+    
+    try {
+        // validate 통과하면 users에 insert data 하기
+        users.concat({email, pw, phone, sex, birth, name});
+        // users 통과하면 200 전달
+        console.log(users);
+        res.sendStatus(201);
+    } catch (e) {
+    // 통과 못하면 res.sendStatus(400)하기
+        res.sendStatus(400);
+    }
 })
 
 // user/login
 router.post('/login', function(req, res){
-    res.send({token: "token", info: {name: "눈송", phone_num: "010-0000-0000"}});
+    // Read username and password from request body
+    const { email, pw } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.email === email && u.pw === pw });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ email: user.email }, accesstokensecret);
+        res.json({
+            accessToken
+        });
+    } else {
+        res.status(400).json({error: 'email or pw incorrect'});
+    }
 })
 
 // user/settings
 router.get('/settings', function(req, res){
+
     res.send({name: "아아", phone_num: "010-0000-0000"});
 })
 
---
+++ src/models/user.js
@@ -0,0 +1,38 @@
+/*
+* EXPLANATION
+* user 관련 tables에 대한 query문들
+*/
+//////////////////////////////////////////////////
+// * IMPORT ZONE
+
+//////////////////////////////////////////////////
+// * User SQLs
+
+/*
+* Users.Model.js
+1. insertUser(data): token='빈문자열'
+2. getEmailPw(email)
+3. updateToken(token)
+4. getUser(user): return UserData
+5. getNamePhone(user)
+6. updateUser(user)
+*/
+
+
+let User = {};
+
+User.insertUser = 'sql문';
+
+User.getEmailPw = 'sql문';
+
+User.updateToken = 'sql문';
+
+User.getUser = 'sql문';
+
+User.getNamePhone = 'sql문';
+
+User.updateUser = 'sql문';
+
+//////////////////////////////////////////////////
+//* EXPORT ZONE
+module.exports = User;
